74524b2cf9d01d608724b7f974fe475481a14cfe
Sebastian Andreasson <sebastianandreasson@Sebastians-MacBook-Pro.local>
2022-03-09 15:42:42 +0100

    comment back real code

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const serial = require('./serial')
+const serial = require('./serial')
 const { Server } = require("socket.io")
 const routes = require('./routes')
 
@@ -17,23 +17,23 @@ const io = new Server(server)
 io.on('connection', socket => {
   socket.on('disconnect', () => console.log('disconnected'))
   // For testing purposes only, send fake duration back to client
-  socket.on('validated', () => {
-    let start = new Date()
-    setTimeout(() => {
-      const duration = new Date() - start
-      io.emit('time', { durationMs: duration })
-    }, 1000 + (100 * Math.random()))
-  })
+  // socket.on('validated', () => {
+  //   let start = new Date()
+  //   setTimeout(() => {
+  //     const duration = new Date() - start
+  //     io.emit('time', { durationMs: duration })
+  //   }, 1000 + (100 * Math.random()))
+  // })
 })
 
-// serial.init({
-//   devpath: process.env.DEVPATH || '/dev/tty.usbserial-D308DE73',
-//   events: {
-//     time: args => {
-//       console.log('Got duration', args[0])
+serial.init({
+  devpath: process.env.DEVPATH || '/dev/tty.usbserial-D308DE73',
+  events: {
+    time: args => {
+      console.log('Got duration', args[0])
 
-//       io.emit('time', { durationMs: +args[0] })
-//     }
-//   }
-// })
+      io.emit('time', { durationMs: +args[0] })
+    }
+  }
+})
 
